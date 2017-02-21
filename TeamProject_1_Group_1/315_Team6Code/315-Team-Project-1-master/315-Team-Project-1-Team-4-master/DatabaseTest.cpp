6628e4b948147131d35ed2dd5533ee9c4153bcd6
tyler-lamkin <tyler_lamkin@email.tamu.edu>
2017-02-21 13:35:19 -0600

    Fixed the repository
    Hopefully for the last time

---
+++ TeamProject_1_Group_1/315_Team6Code/315-Team-Project-1-master/315-Team-Project-1-Team-4-master/DatabaseTest.cpp
@@ -0,0 +1,22 @@
+#include "database.h"
+#include "table.h"
+#include "record.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	Database db();
+	Table t1;
+
+	/*
+	First Error we noticed. Your Record class has no data members, no getters/setters for each of the elements,
+	and the entire class is private. Therefore we cannot create any Record data members.
+	*/
+	Record r1(0), r2(2), r3(4);
+	
+
+
+	// Begin testing Record
+
+
+}
