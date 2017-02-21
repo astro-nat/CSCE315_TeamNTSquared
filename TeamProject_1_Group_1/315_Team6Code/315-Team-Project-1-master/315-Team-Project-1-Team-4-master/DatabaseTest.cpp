355afeeb40a7b1d8c73e1303568eddc65cb46348
tyler-lamkin <tyler_lamkin@email.tamu.edu>
2017-02-21 16:34:34 -0600

    Created the Record test cases
    Created tests for the record functions.

--- TeamProject_1_Group_1/315_Team6Code/315-Team-Project-1-master/315-Team-Project-1-Team-4-master/DatabaseTest.cpp
+++ TeamProject_1_Group_1/315_Team6Code/315-Team-Project-1-master/315-Team-Project-1-Team-4-master/DatabaseTest.cpp
@@ -2,7 +2,11 @@
 #include "table.h"
 #include "record.h"
 #include <iostream>
+#include <string>
 using namespace std;
+
+// GLOBAL TEST VARIABLE
+string TEST = "TEST";
 
 int main() {
 	Database db();
@@ -17,14 +21,50 @@ int main() {
 	
 
 	try {
-		// Begin testing Record
 
+		// Begin testing Record
+		// Test Record.size();
+		if (r1.size() != 0) {
+			throw string("Record size error.\n");
+		}
+		if (r2.size() != 2) {
+			throw string("Record size error.\n");
+		}
+		if (r3.size() != 4) {
+			throw string("Record size error.\n");
+		}
+
+		// Test Basic Initialization
+		// Records should be intialized with an array of empty strings based on the size given
+		if (!r1[0].empty()) {
+			throw string("Record initialization error.\n");
+		}
+		if (!r2[1].empty()) {
+			throw string("Record initialization error.\n");
+		}
+		if (!r3[3].empty()) {
+			throw string("Record initialization error.\n");
+		}
+
+		// Test Getting/Setting values
+		r1[0] = TEST;
+		r2[1] = TEST;
+		r3[3] = TEST;
+		if (r1[0] != TEST) {
+			throw string("Record Set/access failed.\n");
+		}
+		if (r2[1] != TEST) {
+			throw string("Record Set/access failed.\n");
+		}
+		if (r3[3] != TEST) {
+			throw string("Record Set/access failed.\n");
+		}
 		// Begin testing Table
 
 		// Begin testing Database
 	}
-	catch (...) {
-
+	catch (string e) {
+		cout << e;
 	}
 
 	return 0;
