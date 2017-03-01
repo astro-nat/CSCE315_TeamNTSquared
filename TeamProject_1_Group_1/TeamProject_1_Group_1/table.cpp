08fda2687da77524b75209e487bd6514ba48410c
tyler-lamkin <tyler_lamkin@email.tamu.edu>
2017-03-01 17:11:47 -0600

    Updated Natural Join

--- TeamProject_1_Group_1/TeamProject_1_Group_1/table.cpp
+++ TeamProject_1_Group_1/TeamProject_1_Group_1/table.cpp
@@ -131,8 +131,80 @@ using namespace TABLE;
 	}
 
 	Table Table::naturalJoin(Table t1, Table t2) {
-		Table null(0,0);
-		return null;
+		Table joinedTable(0, 0);
+
+		vector<string>t1_attributes;
+		vector<string>t2_attributes;
+		t1_attributes = t1.getAttributes();
+		t2_attributes = t2.getAttributes();
+
+		int biggerSize;
+		int smallerSize;
+
+		if (t1.size() > t2.size()) //get the table with more rows
+		{
+			biggerSize = t1.size();
+			smallerSize = t2.size();
+		}
+		else
+		{
+			biggerSize = t2.size();
+			smallerSize = t1.size();
+		}
+
+		for (int z = 0; z < t1_attributes.size(); z++)
+		{
+			joinedTable.addAttribute(t1_attributes[z]); //add all the attributes of first table into new table
+		}
+
+		for (int z = 0; z < t2_attributes.size(); z++) //add all attributes of second tabe into new table
+		{
+			joinedTable.addAttribute(t2_attributes[z]); //this will make 2 copies of the same attribute (if they share one). 
+		}
+
+
+
+		for (int i = 0; i < biggerSize; i++)
+		{
+			for (int j = 0; j < smallerSize; j++)
+			{
+				if (t1_attributes[i] == t2_attributes[j]) //if the two columns are the same, begin joining tables
+				{
+					for (int m = 0; m < biggerSize; m++)
+					{
+						if (t1.tuples.at(m).at(i) == t2.tuples.at(m).at(j)) //check which rows to join by checking if the content in that row is the same
+						{
+
+							joinedTable.tuples.at(m) = t1.tuples.at(m); //add the whole row in table t1 to the new table
+							joinedTable.tuples.push_back(t2.tuples.at(m)); //append the matched row in table t2 to the new table
+
+							for (int i = 0; i < joinedTable.size(); i++)
+							{
+								if (joinedTable.tuples.at(m).at(i) == joinedTable.tuples.at(m).at(i + 1))
+								{
+									joinedTable.tuples.at(m).erase(i + 1); //since we appended the two rows, they both still share the same value that was equal. need to delete one.
+								}
+							}
+						}
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+
+
+		for (int i = 0; i < joinedTable.size(); i++)
+		{
+			if (joinedTable.attributes[i] == joinedTable.attributes[i + 1])
+			{
+				joinedTable.deleteAttribute(joinedTable.attributes[i + 1]); //delete duplicate attribute (2 same attributes in a row.)
+			}
+		}
+
+		return joinedTable;
 	}
 
 	int Table::count(string attributeName) {
